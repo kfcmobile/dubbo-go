782223f846170b3c2febf21a842e5b26c85788af
zengfanwei <zengfanwei@kaike.la>
2020-12-19 16:29:04 +0800

    add GetInvoker func

--- config/reference_config.go
+++ config/reference_config.go
@@ -252,3 +252,8 @@ func (c *ReferenceConfig) GenericLoad(id string) {
 	c.Refer(genericService)
 	c.Implement(genericService)
 }
+
+// GetInvoker get invoker from ReferenceConfig
+func (c *ReferenceConfig) GetInvoker() protocol.Invoker {
+	return c.invoker
+}
