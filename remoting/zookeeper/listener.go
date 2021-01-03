debb43a33fcbab0997cf18c42b505ad7c3e312ec
zengfanwei <zengfanwei@kaike.la>
2021-01-03 22:06:02 +0800

    rollback

--- remoting/zookeeper/listener.go
+++ remoting/zookeeper/listener.go
@@ -134,19 +134,17 @@ func (l *ZkEventListener) handleZkNodeEvent(zkPath string, children []string, li
 
 	newChildren, err := l.client.GetChildren(zkPath)
 	if err != nil {
-		//if err == errNilChildren {
-		//	content, _, err := l.client.Conn.Get(zkPath)
-		//	if err != nil {
-		//		logger.Errorf("Get new node path {%v} 's content error,message is  {%v}", zkPath, perrors.WithStack(err))
-		//	} else {
-		//		listener.DataChange(remoting.Event{Path: zkPath, Action: remoting.EventTypeUpdate, Content: string(content)})
-		//	}
-		//
-		//} else {
-		//	logger.Errorf("path{%s} child nodes changed, zk.Children() = error{%v}", zkPath, perrors.WithStack(err))
-		//}
-		logger.Errorf("path{%s} child nodes are empty, zk.Children() = error{%v}", zkPath, perrors.WithStack(err))
-		return
+		if err == errNilChildren {
+			content, _, err := l.client.Conn.Get(zkPath)
+			if err != nil {
+				logger.Errorf("Get new node path {%v} 's content error,message is  {%v}", zkPath, perrors.WithStack(err))
+			} else {
+				listener.DataChange(remoting.Event{Path: zkPath, Action: remoting.EventTypeUpdate, Content: string(content)})
+			}
+
+		} else {
+			logger.Errorf("path{%s} child nodes changed, zk.Children() = error{%v}", zkPath, perrors.WithStack(err))
+		}
 	}
 
 	// a node was added -- listen the new node
